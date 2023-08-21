2550fbd5a09fb51396684e692740b4a696bfda69
Cade Meu Code <cademeucode@iMac-de-Abudu.local>
2023-08-22 06:41:26 +0900

    fetch url set to local

--- api/showcase.js
+++ api/showcase.js
@@ -1,6 +1,6 @@
 export async function showcase() {
 	try {
-		const showcaseAccess = await fetch("https://meteora-json-server-tasks.vercel.app/")
+		const showcaseAccess = await fetch("../db.json")
 		let product = await showcaseAccess.json()
 		return product;
 	}
