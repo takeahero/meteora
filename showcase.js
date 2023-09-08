d9c115cf4c9a8ac6923051bc61d6b5738e7fe9aa
Cade Meu Code <cademeucode@iMac-de-Abudu.local>
2023-09-08 21:23:54 +0900

    fix db src

--- showcase.js
+++ showcase.js
@@ -1,6 +1,6 @@
 export async function showcase() {
 	try {
-		const showcaseAccess = await fetch("../db.json")
+		const showcaseAccess = await fetch("./db.json")
 		const product = await showcaseAccess.json()
 		return product
 	}
